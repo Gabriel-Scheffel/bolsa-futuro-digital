a32f3c7c207f12e659b1719b150f2ad2bae17e52
Gabriel Scheffel <gabriel.s.costa01@gmail.com>
2026-09-03 18:00:53 -0300

    Aula 18 quase pronta

---
+++ javascript/aula018/exemplo04/script3.js
@@ -0,0 +1,19 @@
+/*
+// Inicia um contador = 0
+let c = 0
+
+while(c <= 10) {
+    //contar até 10
+    console.log(c)
+    // c = c + 1
+    c++ 
+}
+*/
+
+// (inicia em 1; vai até 10; de 1 em 1)
+for(let c=1; c<=10; c++) {
+    console.log(c)
+}
+console.log('Terminou!')
+
+
