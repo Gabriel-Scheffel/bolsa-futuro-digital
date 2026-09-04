0ce76179899a9bafe2e9d37df65ce505e24bfb8e
Gabriel Scheffel <gabriel.s.costa01@gmail.com>
2026-09-04 10:56:54 -0300

    exercicios aula 19

--- javascript/aula018/exemplo04/script2.js
+++ javascript/aula018/exemplo04/script2.js
@@ -11,6 +11,20 @@ console.log(`Sua vez chegou!`)
 
 
 /*
+let pos = 7
+let fila = 1
+
+// Equanto a fila é menor que a posição
+while (fila < pos) {
+    console.log(`Senha atual: ${fila}.`)
+    fila ++
+}
+
+console.log(`Sua vez chegou!`)
+*/
+
+
+/*
 if (fila == pos) {
 console.log(`Sua vez chegou!`)
 } else {
