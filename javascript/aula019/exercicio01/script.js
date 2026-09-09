22a4ba1b4201ed2c86317233582b813e70d7270c
Gabriel Scheffel <73002431+Gabriel-Scheffel@users.noreply.github.com>
2026-09-09 18:03:13 -0300

    Aula 19 pronta

--- javascript/aula019/exercicio01/script.js
+++ javascript/aula019/exercicio01/script.js
@@ -1,18 +1,24 @@
 function carregar () {
+    // criar as variaveis
     var msg = window.document.getElementById('msg')
     var foto = window.document.getElementById('imagem')
+    // data e hora
     var data = new Date()
     var hora = data.getHours()
+    //var hora = 20
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 0 && hora < 12) {
-        foto.src = ('manha.png')
+        foto.src = ('imagens/manha.png')
         document.body.style.background = '#e2cd9f'
+
     } else if (hora >= 12 && hora < 18) {
-        foto.src = ('tarde.png')
+        foto.src = ('imagens/tarde.png')
         document.body.style.background = '#89cbdf'
+
     } else {
-        foto.src = ('tarde.png')
+        foto.src = ('imagens/noite.png')
         document.body.style.background = '#515154'
+        
     }
 }
--- javascript/aula019/exercicio03/script.js
+++ javascript/aula019/exercicio03/script.js
@@ -3,17 +3,24 @@ function contar() {
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
+
     let res = document.getElementById('res')
 
+    // verifica se os dados foram inseridos
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         res.innerHTML = 'Impossível contar!'
         //window.alert('[ERRO] Faltam dados!')
 
     } else {
+        //window.alert('Tudo OK!)
         res.innerHTML = 'Contando: <br>'
+
+        // Criar as variáveis numéricas
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+
+        // Adicionar o for aqui e só depois o if
         if (p <=0) {
             window.alert('Passo inválido! Considerando PASSO 1')
             p=1
@@ -31,3 +38,5 @@ function contar() {
         res.innerHTML += `\u{1F3C1}`
     }
 }
+
+// Emojis: unicode emoji list
