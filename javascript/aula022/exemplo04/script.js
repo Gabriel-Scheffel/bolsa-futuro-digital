5a0da037b5b6fe0768cff7f9c97b7e65a7edbfe6
Gabriel Scheffel <gabriel.s.costa01@gmail.com>
2026-09-24 18:44:21 -0300

    Aula 22 pronta

---
+++ javascript/aula022/exemplo04/script.js
@@ -0,0 +1,13 @@
+let item = document.querySelector('li#item')
+
+console.log(item.parentNode)
+
+console.log(item.children)
+
+console.log(item.firstElementChild)
+
+console.log(item.lastElementChild)
+
+console.log(item.nextElementSibling)
+
+console.log(item.previousElementSibling)
